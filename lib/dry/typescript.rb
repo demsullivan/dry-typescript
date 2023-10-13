639fc254a71f73333719df3284b247fab4b27e10
Dave Sullivan <dave@davesullivan.ca>
2023-10-13 19:04:46 -0400

    initial commit

---
+++ lib/dry/typescript.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "typescript/version"
+
+module Dry
+  module Typescript
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/dry/typescript/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Dry
+  module Typescript
+    VERSION = "0.1.0"
+  end
+end
