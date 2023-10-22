4599c50e63492acc3c5d6899d212145b7ded0523
Dave Sullivan <dave@davesullivan.ca>
2023-10-22 00:27:49 -0400

    Remove Copilot-written dummy code.

--- lib/dry/typescript.rb
+++ lib/dry/typescript.rb
@@ -19,34 +19,5 @@ module Dry
 
       type_definitions.map(&:to_s)
     end
-
-    def self.generate_type_definitions(types_module)
-      types_module.constants.each do |constant_name|
-        constant = types_module.const_get(constant_name)
-
-        case constant
-        when Module
-          generate_type_definitions(types_module: constant)
-        when Dry::Types::Constrained
-          generate_type_definition_from_constrained(type: constant)
-        when Dry::Types::Sum
-          generate_type_definition_from_sum(type: constant)
-        when Dry::Types::Enum
-          generate_type_definition_from_enum(type: constant)
-        when Class
-          generate_type_definition_from_class(type: constant)
-        else
-          raise Error.new("Unsupported type: #{constant}")
-        end
-      end
-    end
-
-    def self.generate_type_definition_from_constrained(type:)
-      # don't generate type definitions for primitive types
-
-      type_definition = <<~TYPESCRIPT
-        export type #{type.name} = #{type.primitive.name}
-      TYPESCRIPT
-    end
   end
 end
